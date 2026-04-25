875ec42b4ed66008433f6bd18e1383b67a8d0af0
JacoMartins <jaco.alu.lmb@gmail,com>
2026-04-25 18:12:57 -0300

    Initial Commit

---
+++ Word Swapper/Word_SwapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_SwapperApp.swift
+//  Word Swapper
+//
+//  Created by Jacó Alves on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Word_SwapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
